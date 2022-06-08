a585456c33792a2bca861cdf0e24648ed5a42383
Cesar Fernando <totopodev@gmail.com>
2022-06-08 03:08:56 -0500

    adding assignments.js

---
+++ Fundamentals-Part-01/assignments.js
@@ -0,0 +1,22 @@
+/* 
+    Instructions:
+
+    * There is one assignment for each lecture in the JavaScript Fundamentals
+    Sections Part 1 and 2 (not all electures, but most);
+    * The goal of these assignments is that you can immediately apply the concepts
+    you learn in each video;
+    * So after you complete each lecture, find the assignment for the video you just 
+    watched, and write the code according to the instrucions
+    * Take all the time that you need, no need to hurry!
+    * In order to actually write the code, create a new script called assignments.js
+    in the current project folder and link it to the HTML file.
+*/
+
+
+/* JavaScirpt Fundamentals - Part 1 */
+/* LECTURE: Values and Variables
+
+    1. Declare variables called 'country', 'continent and 'population' and
+    assign their values according to your own country (population in millions)
+    2. Log their values to the console
+ */
