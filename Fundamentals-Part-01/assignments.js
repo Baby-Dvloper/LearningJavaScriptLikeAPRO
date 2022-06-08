16888aa2f513963189ff7bce0d31e142934a09eb
Cesar Fernando <totopodev@gmail.com>
2022-06-08 03:15:43 -0500

    assignment 1 completed

--- Fundamentals-Part-01/assignments.js
+++ Fundamentals-Part-01/assignments.js
@@ -20,3 +20,11 @@
     assign their values according to your own country (population in millions)
     2. Log their values to the console
  */
+
+let country = 'Mexico';
+let continent = 'America';
+let population = 129;
+
+console.log(country);
+console.log(continent);
+console.log(population);
