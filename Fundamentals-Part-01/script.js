18d9b1180da654a5f5076c7079ae6d19aee89826
Cesar Fernando <totopodev@gmail.com>
2022-06-09 04:54:21 -0500

    Understanding data types in JavaScript

--- Fundamentals-Part-01/script.js
+++ Fundamentals-Part-01/script.js
@@ -23,3 +23,23 @@ let muCurrentJob = 'Teacher';
 let job1 = 'programmer';
 let job2 = 'teacher';
 */
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year); 
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
