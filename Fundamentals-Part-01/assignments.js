c50dd0510723b9a128aa2fdb15b711b79a46cad2
Cesar Fernando <totopodev@gmail.com>
2022-06-10 11:01:38 -0500

    assignment 2 completed

--- Fundamentals-Part-01/assignments.js
+++ Fundamentals-Part-01/assignments.js
@@ -14,7 +14,7 @@
 
 
 /* JavaScirpt Fundamentals - Part 1 */
-/* LECTURE: Values and Variables
+/* ########### LECTURE: Values and Variables
 
     1. Declare variables called 'country', 'continent and 'population' and
     assign their values according to your own country (population in millions)
@@ -28,3 +28,20 @@ let population = 129;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+
+/* 
+########### Lecture: Data Types
+    1. Declare a varaible called 'isIsland' and set its values according to your
+    country. The variable should hold a Boolean value. Also declare a variable
+    'language', but don't addign it any value yet.
+    2. Log the types of 'isIsland', 'population', 'country' and 'language' ti the
+    console.
+*/
+ let isIsland = false;
+ let language;
+
+ console.log(typeof isIsland);
+ console.log(typeof population);
+ console.log(typeof country);
+ console.log(typeof language);
