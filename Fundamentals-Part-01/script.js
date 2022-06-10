c428ffb6635cd55cb89faafdb48bd3845b405bbd
Cesar Fernando <totopodev@gmail.com>
2022-06-10 11:58:19 -0500

    I learned the different types of operators in JavaScript

--- Fundamentals-Part-01/script.js
+++ Fundamentals-Part-01/script.js
@@ -57,3 +57,32 @@ var lastName = 'Corona';
 console.log(lastName);
 
 */
+
+//Math Operators
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log( ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 ,eamd 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Cesar';
+const lastName = 'Corona';
+console.log(firstName + ' ' + lastName);
+
+// Assignent operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
