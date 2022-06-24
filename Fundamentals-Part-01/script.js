be72884aa43ae96e2db4a574da94f4bc12150c07
Cesar Fernando <totopodev@gmail.com>
2022-06-23 22:43:21 -0500

    I learned the coercion and conversion of types

--- Fundamentals-Part-01/script.js
+++ Fundamentals-Part-01/script.js
@@ -128,7 +128,7 @@ console.log(`String
 multiple
 lines`);
 
-*/
+
 
 const age = 15;
 
@@ -148,3 +148,20 @@ if(birthYear <= 2000){
 }
 
 console.log(century);
+*/
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; //'11'
+n = n - 1; 
+console.log(n);
