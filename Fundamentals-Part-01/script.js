56818c2f8ba1f6f44333e31c31d9a1c47dc9c37d
Cesar Fernando <totopodev@gmail.com>
2022-07-03 20:41:12 -0500

    I learned to use the Equality Operators (loose and strict) and to add more conditions to the if statement.

--- Fundamentals-Part-01/script.js
+++ Fundamentals-Part-01/script.js
@@ -166,7 +166,7 @@ let n = '1' + 1; //'11'
 n = n - 1; 
 console.log(n);
 
-*/
+
 
 // 5 falsy values: 0, '', undefined, null, NaN
 
@@ -190,3 +190,26 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED'); 
 }
+*/
+
+const age = '18';
+if(age === 18) console.log('You just became an adult :D (strict)');
+
+if(age == 18) console.log('You just became an adult :D (loose)');
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if(favorite === 23) {
+    console.log('Cool! 23 is an amazing number!');
+} else if(favorite === 7) {
+    console.log('7 is also a cool number');
+} else if(favorite === 9) {
+    console.log('9 is also a cool number');
+}
+else {
+    console.log('Number is not 23 or 7 or 9');
+}
+
+if(favorite !== 23) console.log('Why not 23?');
