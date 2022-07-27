969438471d895f691b8be3433877959eb8ab805c
Cesar Fernando Corona Morales <totopodev@gmail.com>
2022-07-26 23:47:26 -0500

    assignment 8 completed

--- Fundamentals-Part-01/assignments.js
+++ Fundamentals-Part-01/assignments.js
@@ -119,3 +119,25 @@ console.log('19' - '13' + '17');// '617'
 console.log('19' - '13' + 17); // 23
 console.log('123' < 57); //false
 console.log(5 + 6 + '4' + 9 - 4 - 2);//  1143
+
+/* ####### Lecture: Equality operator: == vs. ===
+    1. Declare a variable 'numNeighbours' based on a prompt input like this: prompt('How many neighbour countries does your country have?');
+    2. If there is  only 1 heighbour, log to the console 'Only 1 border!' (use loose equality == for now)
+    3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours' is greater than 1
+    4. Use an else block to log  'No borders' (this block will be executed when 'numNeighbours' is 0 or any other value)
+    5. Test the code with deffirent values of 'numNeighbours'm including 1 and 0.
+    6. Change == to ===, and test the code again, with the same values of 'numNeighbours'. Notice what happens when thre is exactly q border! Qhy is this happening?
+    7. Finally, convert'numNeighbours' to a number, and watch what happens now when you input 1
+    8. Reflect on why we should use the === and type conversion in this situation
+*/
+
+
+const numNeighbours =Number(prompt('How many neighbour contries does your country have?'));
+
+if(numNeighbours === 1) {
+    console.log('Only 1 border!'); 
+} else if(numNeighbours > 1) {
+    console.log('More than 1 border')
+} else {
+    console.log('No borders');
+}
