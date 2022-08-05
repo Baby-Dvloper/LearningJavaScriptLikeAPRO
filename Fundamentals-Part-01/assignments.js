a2d3db5c928b53fc8a11b4e976f96d240b38cb48
CesarDev <totopodev@gmail.com>
2022-08-05 03:14:01 -0500

    assignment 9 completed

--- Fundamentals-Part-01/assignments.js
+++ Fundamentals-Part-01/assignments.js
@@ -132,7 +132,7 @@ console.log(5 + 6 + '4' + 9 - 4 - 2);//  1143
 */
 
 
-const numNeighbours =Number(prompt('How many neighbour contries does your country have?'));
+/*const numNeighbours =Number(prompt('How many neighbour contries does your country have?'));
 
 if(numNeighbours === 1) {
     console.log('Only 1 border!'); 
@@ -140,4 +140,19 @@ if(numNeighbours === 1) {
     console.log('More than 1 border')
 } else {
     console.log('No borders');
+}
+*/
+
+/* ####### Lecture: Logical Operators
+    1. Comment out the previous code so the prompt doesn't get in the way
+    2. Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks english, has less than 50 million people and is not an island.
+    3. Write an if statement to help Sarah figure out if your country is right for her. You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution id necessary.
+    4. If yours is the right country, log a string like this: 'You should like in Portugal:)'. If not, log 'Portugal does not meet your criteria :('
+    5. Probably your country does not meet all the criteria. So go back and temporarily change some variables in order to make the condition true (unless you live in Canada :D)
+*/
+
+if (language === 'english' && population < 50 && !isIsland) {
+  console.log(`You should live in ${country}:)`);
+} else {
+  console.log(`${country} does not meet your criteria :(`);
 }
