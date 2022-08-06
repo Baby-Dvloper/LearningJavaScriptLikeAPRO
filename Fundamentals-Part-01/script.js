5a017aab4f0f19e9ca65dacc94dfc1cd7d58a116
CesarDev <totopodev@gmail.com>
2022-08-06 01:45:51 -0500

    I have learned how to implement the switch statement in my code and see the differences between it and the else if statement

--- Fundamentals-Part-01/script.js
+++ Fundamentals-Part-01/script.js
@@ -212,7 +212,7 @@ else {
 }
 
 if(favorite !== 23) console.log('Why not 23?');
-*/
+
 
 const hasDriverLicense = true; // A
 const hasGoodVision = true; // B
@@ -222,11 +222,11 @@ console.log(hasDriverLicense || hasGoodVision);
 console.log(!hasDriverLicense);
 
 
-/*if(hasDriverLicense && hasGoodVision) {
-    console.log('Sarah is able to drive!');
-} else {
-    console.log('Someone else should drive...');
-}*/
+// if(hasDriverLicense && hasGoodVision) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
 
 const isTired = false; // C
 console.log(hasDriverLicense && hasGoodVision && isTired);
@@ -235,4 +235,47 @@ if(hasDriverLicense && hasGoodVision && !isTired) {
     console.log('Sarah is able to drive!');
 } else {
     console.log('Someone else should drive...');
+}
+
+*/
+
+const day = 'thursday';
+
+switch(day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+
+if(day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if(day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if(day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if(day === friday) {
+  console.log('Record videos');
+} else if(day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend');
+} else {
+  console.log('Not a valid day!');
 }
