c3feb3f5b1a21ff83535ef4f4ddd14ed2bdda23c
CesarDev <totopodev@gmail.com>
2022-08-12 01:37:44 -0500

    assignment 1 completed

---
+++ Fundamentals-Part-02/assignments.js
@@ -0,0 +1,17 @@
+// ########### Lecture: Functions
+/*
+	1. Write a function called 'describeCountry' which takes three parameters: 'country', 'population' and 'capitalCity'. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'
+	2. Call this funtion 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console 
+*/
+
+function describeCountry(country, population, capitalCity) {
+	return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const mexico = describeCountry('Mexico' , 129, 'Mexico City');
+const newZealand = describeCountry('New Zealand' , 5, 'Wellington');
+const uzbekistan = describeCountry('Uzbekistan' , 34, 'Tashkent');
+
+console.log(mexico);
+console.log(newZealand);
+console.log(uzbekistan);
