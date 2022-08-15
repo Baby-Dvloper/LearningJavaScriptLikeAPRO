8c7ae799c238da324dce18d4c10e8920dc36f695
Cesar Fernando <totopodev@gmail.com>
2022-08-15 17:48:09 -0500

    assignment 2 completed

--- Fundamentals-Part-02/assignments.js
+++ Fundamentals-Part-02/assignments.js
@@ -15,3 +15,32 @@ const uzbekistan = describeCountry('Uzbekistan' , 34, 'Tashkent');
 console.log(mexico);
 console.log(newZealand);
 console.log(uzbekistan);
+
+// ############# Lecture: Function Declarations vs. Expresions
+/*
+	1. The world population is 7900 million people. Create a funtion declaration called 'percentagesOfWorld1' which receives a 'population' value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population
+	2. To calculate the percentage, divide the given 'population' value by 7900 and then multiply by 100
+	3. Call 'percentageOfWorld1' for 3 populations of countries of your choice, store the results into variables, and log them to the console.
+	4. Create a function expression which does the exact same thing, called 'percentageOfWorld2', and also call it with 3 country propulations (can be the same populations)
+*/
+
+function percentageOfWorld1(population) {
+	return population / 7900 * 100;
+}
+
+const mexicopopulation1 = percentageOfWorld1(129);
+const newZealandpopulation1 = percentageOfWorld1(5);
+const uzbekistanpopulation1 = percentageOfWorld1(34);
+
+
+console.log(mexicopopulation1, newZealandpopulation1, uzbekistanpopulation1);
+
+const percentageOfWorld2 = function (population) {
+	return population / 7900 * 100;
+}
+
+const mexicopopulation2 = percentageOfWorld2(129);
+const newZealandpopulation2 = percentageOfWorld2(5);
+const uzbekistanpopulation2 = percentageOfWorld2(34);
+
+console.log(mexicopopulation2, newZealandpopulation2, uzbekistanpopulation2);
