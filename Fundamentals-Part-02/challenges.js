a12508884d6a29b2e1c0eb789a775841b8e55f99
Cesar Fernando Corona <totopodev@gmail.com>
2023-01-23 02:21:01 -0600

    challenge 2 (section 2) complete

--- Fundamentals-Part-02/challenges.js
+++ Fundamentals-Part-02/challenges.js
@@ -1,4 +1,7 @@
 'use strict';
+/*
+///////////////////////////////////////////////////////////////////
+// ######## Challenge 1
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
 const checkWinner = function (avgDolphins, avgKoalas) {
@@ -19,3 +22,22 @@ const averageKoalas = calcAverage(23, 34, 27);
 
 checkWinner(averageDolphins, averageKoalas);
 console.log(averageDolphins, averageKoalas);
+*/
+
+const calcTip = function (bill) {
+  // if(bill >= 50 && bill <= 300) {
+  //   return bill * 0.15;
+  // } else {
+  //   return bill * 0.2;
+  // }
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  
+}
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+
+console.log(total);
