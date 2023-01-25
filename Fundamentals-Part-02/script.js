35c383e186eb5afe4918e7167dc56991299d085e
Cesar Fernando Corona <totopodev@gmail.com>
2023-01-24 22:50:25 -0600

    I Learned to query  properties from an object with the dot and bracket notation and the differences between them

--- Fundamentals-Part-02/script.js
+++ Fundamentals-Part-02/script.js
@@ -187,6 +187,7 @@ if (friends.includes('Steven')) {
 */
 
 /*################ Introduction to objects*/
+/*
 const jonas = {
 	firstName: 'Jonas',
 	lastName: 'Schmedtmann',
@@ -194,4 +195,35 @@ const jonas = {
 	job: 'teacher',
 	friends: ['Michael', 'Peter', 'Steven']
 };
+*/
+
+const jonas = {
+	firstName: 'Jonas',
+	lastName: 'Schmedtmann',
+	age: 2037 - 1991,
+	job: 'teacher',
+	friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if(jonas[interestedIn]) {
+	console.log(jonas[interestedIn]);
+} else {
+	console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 
