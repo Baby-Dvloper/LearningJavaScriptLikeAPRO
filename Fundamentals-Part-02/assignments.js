3efd1bc3c4e283f503ddb2db9b0d25cd6f423ac8
Cesar Fernando Corona <totopodev@gmail.com>
2023-02-12 02:15:00 -0600

    assignment 10 completed

--- Fundamentals-Part-02/assignments.js
+++ Fundamentals-Part-02/assignments.js
@@ -113,3 +113,9 @@ console.log(myCountry['population']);
 myCountry.describe();
 myCountry.checkIsland();
 console.log(myCountry);
+
+// #################### Lecture: Iteration - The for Loop
+
+for(let voter = 1; voter <= 50; voter++) {
+  console.log(`Voter number ${voter} is currently voting`);
+}
