725d6fda30af3eb20c6b69f6a4791cb5bdf23752
Cesar Fernando Corona <totopodev@gmail.com>
2023-02-16 21:32:06 -0600

    lecture 48 finished

--- Fundamentals-Part-02/script.js
+++ Fundamentals-Part-02/script.js
@@ -291,6 +291,7 @@ for(let rep = 1; rep <= 10; rep++) {
 */
 
 // #################  Looping Arrays, Breaking and Continuing
+/*
 const jonas = [
 	'Jonas',
 	'Schmedtmann',
@@ -332,5 +333,26 @@ for(let i = 0; i < jonas.length; i++) {
 	if(typeof jonas[i] === 'number') break;
 	console.log(jonas[i], typeof jonas[i]);
 }
+*/
+//########################### Looping Backwards and Loops in Loops 
 
- 
+const jonas = [
+	'Jonas',
+	'Schmedtmann',
+	2037 - 1991,
+	'teacher',
+	['Michael', 'Peter', 'Steven'],
+	true 
+];
+
+for(let i = jonas.length - 1; i >= 0; i--) {
+	console.log(i, jonas[i]);
+}
+
+for(let exercise = 1; exercise < 4; exercise++) {
+	console.log(`---------- Starting exercise ${exercise}`);
+
+	for(let rep = 1; rep < 6; rep++) {
+		console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋`);
+	}
+}
