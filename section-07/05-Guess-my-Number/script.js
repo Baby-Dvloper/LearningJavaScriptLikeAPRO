0280db8db65db53870d710252ae4f4edfddfdc12
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-05-08 04:40:46 -0600

    Lecture 76 completed

--- section-07/05-Guess-my-Number/script.js
+++ section-07/05-Guess-my-Number/script.js
@@ -10,9 +10,8 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //When the player wins
   } else if (secretNumber === guess) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -53,4 +53,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
     // document.querySelector('.score').textContent = currentScore -= 1; //Codigo prueba
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
