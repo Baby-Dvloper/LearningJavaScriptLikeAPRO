a5927db899ec425727ec2370fb346d40f8c3bbb5
Cesar Fernando <totopodev@gmail.com>
2023-06-07 10:15:04 -0600

    Lecture 81 completed

--- section-07/Modal/script.js
+++ section-07/Modal/script.js
@@ -5,7 +5,6 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -23,3 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
