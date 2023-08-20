8655bf5115e87c8e4ed56c1512b268f538d9bfcf
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-08-19 19:46:19 -0600

    Lecture 'Hoisting and TDZ in practice' completed

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -1,32 +1,77 @@
 'use strict';
 
-function calcAge(birthYear) {
-	const age = 2037 - birthYear;
-	function printAge() {
-		let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-		console.log(output);
+// function calcAge(birthYear) {
+// 	const age = 2037 - birthYear;
+// 	function printAge() {
+// 		let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+// 		console.log(output);
 
-		if (birthYear >= 1981 && birthYear <= 1996) {
-			var millenial = true;
-			// Creating NEW varaible with same name as outer scope's variable
-			const firstName = 'Steven';
-			// Reassingning outer scope's variable
-			output = 'NEW OUTPUT!';
-			const str = `Oh, and you are a millenial, ${firstName}`;
-			console.log(str);
-			function add(a, b) {
-				return a + b;
-			}
-		}
-		// console.log(str);
-		console.log(millenial);
-		// add(2, 3);
-		console.log(output);
-	}
+// 		if (birthYear >= 1981 && birthYear <= 1996) {
+// 			var millenial = true;
+// 			// Creating NEW varaible with same name as outer scope's variable
+// 			const firstName = 'Steven';
+// 			// Reassingning outer scope's variable
+// 			output = 'NEW OUTPUT!';
+// 			const str = `Oh, and you are a millenial, ${firstName}`;
+// 			console.log(str);
+// 			function add(a, b) {
+// 				return a + b;
+// 			}
+// 		}
+// 		// console.log(str);
+// 		console.log(millenial);
+// 		// add(2, 3);
+// 		console.log(output);
+// 	}
 
-	printAge();
-	return age;
+// 	printAge();
+// 	return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+// ##########################################################################
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+	return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
+const addExpr = function (a, b) {
+	return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+function deleteShoppingCart() {
+	console.log('All product deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
