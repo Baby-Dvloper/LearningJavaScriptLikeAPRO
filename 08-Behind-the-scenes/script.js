049a29270b3c57c770a1aec17af63815da4a7c09
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-08-21 21:39:50 -0600

    Lecture 'Primitives vs. Objects(Primitive vs. Reference Types)' completed

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -114,49 +114,67 @@
 
 // ##############################################
 
-const jonas = {
-	firstName: 'Jonas',
-	year: 1991,
-	calcAge: function () {
-		// console.log(this);
-		console.log(2037 - this.year);
+// const jonas = {
+// 	firstName: 'Jonas',
+// 	year: 1991,
+// 	calcAge: function () {
+// 		// console.log(this);
+// 		console.log(2037 - this.year);
 
-		// Solution 1
-		// const self = this;
-		// const isMillenial = function () {
-		// 	console.log(self);
-		// 	console.log(self.year >= 1981 && self.year <= 1996);
-		// };
+// 		// Solution 1
+// 		// const self = this;
+// 		// const isMillenial = function () {
+// 		// 	console.log(self);
+// 		// 	console.log(self.year >= 1981 && self.year <= 1996);
+// 		// };
 
-		// Solution 2
-		const isMillenial = () => {
-			console.log(this);
-			console.log(this.year >= 1981 && this.year <= 1996);
-		};
-		isMillenial();
-	},
+// 		// Solution 2
+// 		const isMillenial = () => {
+// 			console.log(this);
+// 			console.log(this.year >= 1981 && this.year <= 1996);
+// 		};
+// 		isMillenial();
+// 	},
 
-	greet: () => {
-		console.log(this);
-		console.log(`Hey ${this.firstName}`);
-	},
+// 	greet: () => {
+// 		console.log(this);
+// 		console.log(`Hey ${this.firstName}`);
+// 	},
+// };
+
+// jonas.greet();
+// jonas.calcAge();
+
+// // arguments keyword
+// const addExpr = function (a, b) {
+// 	console.log(arguments);
+// 	return a + b;
+// };
+
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12);
+
+// const addArrow = (a, b) => {
+// 	console.log(arguments);
+// 	return a + b;
+// };
+
+// addArrow(2, 5, 8);
+
+// ##############################################
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+	name: 'Jonas',
+	age: 30,
 };
 
-jonas.greet();
-jonas.calcAge();
-
-// arguments keyword
-const addExpr = function (a, b) {
-	console.log(arguments);
-	return a + b;
-};
-
-addExpr(2, 5);
-addExpr(2, 5, 8, 12);
-
-const addArrow = (a, b) => {
-	console.log(arguments);
-	return a + b;
-};
-
-addArrow(2, 5, 8);
+const friend = me;
+friend.age = 27;
+console.log('Friend', friend);
+console.log('Me', me);
