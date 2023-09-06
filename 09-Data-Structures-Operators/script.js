1d832c03f4bf0d26c0114247a90d8e47de78f3dc
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-09-06 17:58:28 -0600

    Lecture 'Logical Assignment Operators' completed

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -64,6 +64,39 @@ const restaurant = {
 	},
 };
 
+const rest1 = {
+	name: 'Capri',
+	// numGuests: 20,
+	numGuests: 0,
+};
+
+const rest2 = {
+	name: 'La Piazza',
+	owner: 'Giovanni Possi',
+};
+
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+// ##################################################
+// The Nullish Coalescing operator
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -71,6 +104,7 @@ console.log(guests);
 // Nullish, null and undefined (NOT 0 or '')
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
+*/
 /*
 // ###########################################################
 // Short Circuiting (&& and ||)
