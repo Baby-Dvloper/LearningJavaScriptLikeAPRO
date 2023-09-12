46c3ec1ec0cf2fbf51806643fb19034ca1ac9b6e
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-09-12 10:14:24 -0600

    Lecture 'Maps Fundamentals' completed

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -70,6 +70,40 @@ const restaurant = {
 	},
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+	.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+	.set('open', 11)
+	.set('close', 23)
+	.set(true, 'We are open :D')
+	.set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.set(document.querySelector('h1'), 'Heading'));
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+// #####################################################
+// Sets
 const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
 
 console.log(ordersSet);
@@ -93,6 +127,7 @@ console.log(staffUnique);
 console.log(new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']).size);
 
 console.log(new Set('jonasschmedtmann').size);
+*/
 /*
 // #########################################################
 // Looping Objects: Objects Keys, Values,and Entries
