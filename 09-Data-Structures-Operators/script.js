0e6c93e584f69a096dd9af69fd4db19da357ba80
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-09-12 00:15:17 -0600

    Lecture 'Sets' completed

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -70,6 +70,33 @@ const restaurant = {
 	},
 };
 
+const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
+
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread')); // similar to array.includes
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']).size);
+
+console.log(new Set('jonasschmedtmann').size);
+/*
+// #########################################################
+// Looping Objects: Objects Keys, Values,and Entries
+
 // property NAMES
 const properties = Object.keys(openingHours);
 console.log(properties);
@@ -93,6 +120,7 @@ const entries = Object.entries(openingHours);
 for (const [key, { open, close }] of entries) {
 	console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
+*/
 /*
 // ########################################################
 // Optional Chaining
