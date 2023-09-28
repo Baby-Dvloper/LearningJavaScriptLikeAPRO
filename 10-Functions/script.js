569bcfca75ddba186c19e6842d3c03b2bd96ef6a
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-09-27 20:41:34 -0600

    Lecture 'Default Parameters' completed

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // numPassengers || (numPassengers = 1); // mine
+  // price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH428', 2);
+createBooking('LH428', 5);
+createBooking('LH428', undefined, 1000); // skip a parameter
