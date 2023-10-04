10cebbdf86d96b4a8867794c41e371d2ed061e9b
Cesar Fernando Corona Morales <totopodev@gmail.com>
2023-10-03 22:31:47 -0600

    Lecture 'How Passing Arguments Works: Value vs. Reference' completed

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+// Default Parameters
 const bookings = [];
 
 const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
@@ -21,3 +23,35 @@ createBooking('LH123', 2, 800);
 createBooking('LH428', 2);
 createBooking('LH428', 5);
 createBooking('LH428', undefined, 1000); // skip a parameter
+*/
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 2343141451312,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 2343141451312) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
